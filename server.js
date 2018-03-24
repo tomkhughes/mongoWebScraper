79c8dda4965e27d79340d147cf381dfc30b01dc5
Tom Hughes <tomkhughes@g.cofc.edu>
2018-03-24 10:16:49 -0700

    heroklu probs

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.use(express.static("public"));
 // mongoose.connect("mongodb://localhost/scraper", {
 //     useMongoClient: true
 // });
+
+
 if(process.env.MONGODB_URI){
   mongoose.connect(process.env.MONGODB_URI, function(){
     console.log("uri connected");
